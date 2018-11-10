ecc56162b294383b5973685ea8641a35e05e7f93
Brandon Geraci <bran0847@d.umn.edu>
2018-11-10 02:22:16 -0600

    Updated some of the links to my website

--- routes/education.js
+++ routes/education.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET experience page. */
+/* GET education page. */
 router.get('/', function(req, res, next) {
-    res.render('experience', { title: 'Work History' });
+    res.render('education', { title: 'Work History' });
 });
 
 module.exports = router;
